b7b2fceae3ee4c31d31d1c5591fb88d011903216
André Gois <andresousalee@gmail.com>
2022-05-25 20:07:45 -0300

    :recycle:	refactor: remove comments

--- src/modulos/Post/repository/PostRepository.js
+++ src/modulos/Post/repository/PostRepository.js
@@ -5,19 +5,15 @@ async function listAllPost(){
 }
 
 async function createPost( descricao ){
-    
-    /*const end = await prisma.address.create({
-
-    })*/
 
     const result = await prisma.post.create({
         data: {
             descricao: descricao,
-                   }
+        }
     });
     
     return result;
 }
 
 
-module.exports = { listAllUsers, createUser }
+module.exports = { listAllPost, createPost }

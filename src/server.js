f0b7404d4bbaf586e53a9aa2f5eb643612ae861e
André Gois <andresousalee@gmail.com>
2022-05-25 20:06:57 -0300

    :truck:	chore: removing middleware from main file

--- src/server.js
+++ src/server.js
@@ -4,23 +4,6 @@ require('express-async-errors');
 const app = require('./routes')
 const config = require('@config');
 
-app.use(express.json())
-
-app.use( (error, req, res, next) => {
-    if( error instanceof Error){
-        return res.status(404).json({
-            status: 'error',
-            message: `${error.message}`,
-        })
-    }
-
-    return res.status(500).json({
-        status: 'error',
-        message: `Internal server error - ${error.message}`,
-    })
-    //next(error);
-})
-
 app.listen(config.app.port, () => {
     console.log('Server is running!')
 })

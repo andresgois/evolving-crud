6fda04c388e06b7139e17b3dbf13ac8203a79a2c
André Gois <andresousalee@gmail.com>
2022-05-26 13:57:02 -0300

    :sparkles:	feat: adding route to view stored images

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const UserController = require('../modulos/User/controller/UserController');
 const route = express();
-
+const multerConfig = require('../helps/upload')
 const path = require('path')
 const multer = require('multer');
-route.use('/public',express.static('public'));
+//route.use('/public',express.static('public'));
+const verifyImage = require('../modulos/middleware/verifyImage');
+route.use('/files', express.static(path.resolve(__dirname, '..','..','public')))
+//const uploadsFolder = path.resolve(__dirname, '..', '..', 'public')
 
-const uploadsFolder = path.resolve(__dirname, '..', '..', 'public')
-
-const storage = multer.diskStorage({
+/*const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, uploadsFolder)
     },
     filename: function (req, file, cb) {
       cb(null, file.fieldname + '-' + file.originalname)
     }
-  })
-  
-  const upload = multer({ storage: storage })
+})
+
+const upload = multer({ storage: storage })*/
 
 const userController = new UserController();
 
@@ -25,7 +26,8 @@ route.get('/', userController.listAll);
 
 route.get('/:id', userController.listOne);
 
-route.post('/', upload.single('file'), userController.create);
+//route.post('/', upload.single('file'), userController.create);
+route.post('/', multer(multerConfig).single('file'), verifyImage, userController.create);
 
 route.put('/:id', userController.update);
 

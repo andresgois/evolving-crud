5c9559c7c2554767ffb3748af27d10c7bf0ef616
André Gois <andresousalee@gmail.com>
2022-05-31 22:03:31 -0300

    :truck:	chore: remove middleware delete image

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,25 +1,13 @@
 const express = require('express');
 const UserController = require('../modulos/User/controller/UserController');
 const route = express();
-const multerConfig = require('../helps/upload')
 const path = require('path')
 const multer = require('multer');
-//route.use('/public',express.static('public'));
+const multerConfig = require('../helps/upload')
 const verifyImage = require('../modulos/middleware/verifyImage');
-const removeImage = require('../modulos/middleware/removeImage');
+
+
 route.use('/files', express.static(path.resolve(__dirname, '..','..','public')))
-//const uploadsFolder = path.resolve(__dirname, '..', '..', 'public')
-
-/*const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, uploadsFolder)
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + file.originalname)
-    }
-})
-
-const upload = multer({ storage: storage })*/
 
 const userController = new UserController();
 
@@ -27,12 +15,11 @@ route.get('/', userController.listAll);
 
 route.get('/:id', userController.listOne);
 
-//route.post('/', upload.single('file'), userController.create);
 route.post('/', multer(multerConfig).single('file'), verifyImage, userController.create);
 
 route.put('/:id', userController.update);
 
-route.delete('/:id', removeImage, userController.destroy);
+route.delete('/:id', userController.destroy);
 
 
 module.exports = route;

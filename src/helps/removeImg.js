2a583386ff67c922a50cdb7c2a2848208aa8eef9
André Gois <andresousalee@gmail.com>
2022-05-31 22:00:50 -0300

    :sparkles:	feat: create help for delete image

---
+++ src/helps/removeImg.js
@@ -0,0 +1,25 @@
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+
+function removerImg(imagem) {
+    console.log("imagem: "+imagem)
+    console.log('LOGGED');
+    if(!imagem){
+        return;
+    }
+    if(process.env.STORAGE_TYPE === 's3'){
+        return s3.deleteObject({
+            Bucket: process.env.AWS_BUCKET,
+            Key: imagem,
+        }).promise();
+    } else {
+        return promisify(fs.unlink)(
+            path.resolve(__dirname, '..','..','public', imagem)
+        )
+    }
+  };
+
+  module.exports = removerImg;

d6474bf469143f6971cfb3b0f6140abed3534eca
André Gois <andresousalee@gmail.com>
2022-06-03 20:06:20 -0300

    :sparkles:	feat: authenticate to user to admin

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,27 +1,32 @@
 const express = require('express');
-const UserController = require('../modulos/User/controller/UserController');
 const route = express();
 const path = require('path')
 const multer = require('multer');
 const multerConfig = require('../helps/upload')
-const Authenticate = require('../modulos/middleware/auth')
-const isAdmin = require('../modulos/middleware/isAdmin')
-
-route.use('/files', express.static(path.resolve(__dirname, '..','..','public')))
+const Authenticate = require('@Middleware/auth')
+const isAdmin = require('@Middleware/isAdmin')
+const UserController = require('../modulos/User/controller/UserController');
 
 const userController = new UserController();
 
-route.get('/', isAdmin, userController.listAll);
+route.use('/files', express.static(path.resolve(__dirname, '..','..','public')))
+
+route.get('/', userController.listAll);
 
 route.get('/:id', userController.listOne);
 
-route.post('/', multer(multerConfig).single('file'), userController.create);
+route.post('/', Authenticate, multer(multerConfig).single('file'), userController.create);
 
-route.put('/:id', userController.update);
+route.put('/:id', Authenticate, userController.update);
 
-route.delete('/:id', userController.destroy);
+route.delete('/:id', Authenticate, userController.destroy);
 
-route.put('/admin/:id', userController.turnIntoAdministrator);
+route.put(
+    '/admin/:id',
+    Authenticate,
+    isAdmin,
+    userController.turnIntoAdministrator
+);
 
 
 module.exports = route;

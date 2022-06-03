f6dedff29372c772aa7d23fb28651987e207542e
André Gois <andresousalee@gmail.com>
2022-06-03 20:09:41 -0300

    :sparkles:	feat: creating middleware authenticate

---
+++ src/middleware/auth.js
@@ -0,0 +1,22 @@
+const { verify } = require('jsonwebtoken')
+
+module.exports = function (req, res, next) {
+  console.log('Auth')
+  const authHeader = req.headers.authorization;
+
+  if(!authHeader){
+    throw new Error("Token missing!");    
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const d = verify(token, process.env.JWT_SECRET);
+    //console.log(d)
+
+    next();
+  } catch(e) {
+    throw new Error("Invalid token!");
+  }
+
+}

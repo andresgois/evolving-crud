e0b6cff899a476e6cef877333f28d43e4a8c4f4e
André Gois <andresousalee@gmail.com>
2022-06-07 19:27:33 -0300

    :sparkles:	feat: update post

--- src/modulos/Post/repository/PostRepository.js
+++ src/modulos/Post/repository/PostRepository.js
@@ -1,7 +1,8 @@
 const prisma = require("../../../database/prismaClient");
 
 async function listAllPost(){
-   const result = await prisma.post.findMany()
+   const result = await prisma.post.findMany();
+   return result;
 }
 
 async function createPost( descricao ){
@@ -15,5 +16,20 @@ async function createPost( descricao ){
     return result;
 }
 
+async function updatePost( id, descricao ){
+    const result = await prisma.post.update({
+        where: { id: id},
+        data: { descricao: descricao}
+    });
+    return result;
+}
 
-module.exports = { listAllPost, createPost }
+async function deletePost( id ) {
+    const result = await prisma.post.delete({
+        where: { id: id}
+    });
+    return result;
+}
+
+
+module.exports = { listAllPost, createPost, updatePost, deletePost }

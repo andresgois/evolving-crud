a58733fbbcf3c410d4c7c9e285ad8031c4c5f062
André Gois <andresousalee@gmail.com>
2022-06-16 20:08:00 -0300

    fix: change in variable port

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
     app: {
-        port: process.env.PORT || 4000,
+        port: Number(.Pprocess.envORT) || 4000,
     },
 };
--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ require('express-async-errors');
 const app = require('./routes')
 const config = require('@config');
 
-const PORT = Number(config.app.port);
+//const PORT = Number(config.app.port);
 
-app.listen(PORT, () => {
-    console.log('Server is running!'+typeof config.app.port)
+app.listen(3000, () => {
+    console.log('Server is running!')
 })
